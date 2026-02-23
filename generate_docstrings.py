043e02845aafc0b038f389f7358e0c4605e63f88
rien333 <rien334@gmail.com>
2026-02-23 07:50:41 +0100

    gen_docstrings: generate cardinality info

--- generate_docstrings.py
+++ generate_docstrings.py
@@ -1,14 +1,19 @@
+#!/bin/python
+
 import dataclasses
 import xml.etree.ElementTree as ET
 import ORI_A
 import textwrap
+import annotationlib
+from typing import Union, get_origin, get_args
 
 from jinja2 import Template
 
+MAX_WIDTH = 87
+INDENT = 4
+
 ns = {"xs": "http://www.w3.org/2001/XMLSchema"}
 root = ET.parse("ORI-A.xsd").getroot()
-MAX_WIDTH = 82
-INDENT = 4
 
 with open('ORI_A/ORI_A.template.py') as f:
     template = Template(f.read())
@@ -40,19 +45,43 @@ for complex_type in root.findall(".//xs:complexType", namespaces=ns):
 
     for field in python_ordered_fields:
         elem = complex_type.find(f"./xs:sequence/xs:element[@name='{field.name}']", namespaces=ns)
+        optional = not int(elem.attrib["minOccurs"])
+        repeatable = True if elem.attrib["maxOccurs"] == "unbounded" else False
+
+        if isinstance(field.type, annotationlib.ForwardRef):
+            # nested dataclasses need the replace, for some reason
+            field_type_name = f"{field.type.__forward_arg__}".replace(
+                " | __annotationlib_name_1__", ""
+            )
+        elif get_origin(field.type) is Union:
+import Ori_A            field_type_name = get_args(field.type)[0].__name__
+        else:
+            field_type_name = field.type.__name__
+
+        if repeatable and optional:
+            cardinality = "[0..*]"
+        elif not repeatable and optional:
+            cardinality = "[0..1]"
+        elif repeatable and not optional:
+            cardinality = "[1..*]"
+        elif not repeatable and not optional:
+            cardinality = "[1..1]"
+
         field_docstring = elem.find("./xs:annotation/xs:documentation", namespaces=ns).text
+
         field_docstring = textwrap.fill(
-            f"{field.name}: {field_docstring}",
+            f"{field.name} ({field_type_name}{cardinality}): " \
+            f"{field_docstring}",
             width=MAX_WIDTH,
             initial_indent=" "*(2*INDENT),
             subsequent_indent=" "*(2*INDENT+2),
         )
+
         class_docstring += field_docstring + "\n"
 
     class_docstring += " "*INDENT
-    # print(class_docstring)
     docs[class_name[0].lower() + class_name[1:]] = class_docstring
 
-with open("ORI_A/ORI_A.py", "w") as f:
+with open("ORI_A/ORI_A.py", "w+") as f:
     f.write(template.render(docs=docs))
 
